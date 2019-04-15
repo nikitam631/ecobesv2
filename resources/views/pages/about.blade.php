4484347df0c7184ab8b2a13628d2bda077f51d7b
nikitam631 <nikita232806@gmail.com>
2019-04-16 01:18:51 +0530

    about page modified

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -8,125 +8,188 @@
 
 
     <!-- FONT AWESOME -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <link rel="stylesheet" href="css/about.css">
   </head>
   <body>
 
-    <!-- TOPBAR -->
 
-    <section class="about-top">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-12">
-            <img src="{{ url('img/logo.png') }}" alt="Ecobes" class="img-responsive">
-          </div>
-
-        </div>
-      </div>
-    </section>
-    <!-- TOPBAR -->
 
     <!-- BANNER SECTION -->
     <section class="about-banner">
-      <div class="container-fluid">
+      <!-- TOPBAR -->
+      <section class="about-top">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-2 inline no-pad">
+              <img src="{{ url('img/about-logo.png') }}" alt="Ecobes" class="img-responsive">
+              {{-- <h1 class="about-top-heading">Company Overview</h1> --}}
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- TOPBAR -->
+      <div class="container">
         <div class="row">
-          <div class="col-md-3">
-              <div class="banner-box">
-                <p class="text-center">Mission</p>
-                <p class="mission">"Empowerment through wisdom, to encourage environmental resilience accross the globe."</p>
-              </div>
+          <div class="col-md-12">
+            <h1 class="tagline">IT IS BETTER WHEN TOGETHER</h1>
           </div>
         </div>
       </div>
     </section>
     <!-- BANNER SECTION -->
 
-    <!-- ABOUT BOX -->
-    <section class="about-box">
-      <div class="container-fluid bg-green">
-        <div class="row">
-          <div class="col-md-12">
-            <h1>About Eco-Bes</h1>
-            <p>Our mission is what drives us to take this enterprising path to Inspire, Motivate and at the same time educate people of all ages across the globe, to become consciously aware and well informed about the current and predicted Global climatic and environmental status. This we strongly believe has the potential to help in building and establishing an unified perspective about the urgency and need for sustainable development practices followed by actions.</p>
-            <p><b>“We are the first generation to feel the effect of climate change and the last generation who can do something about it”-Barack Obama</b></p>
-          </div>
+    <!-- ABOUT US MENU -->
+    <nav class="navbar navbar-green">
+      <div class="container-fluid no-pad">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">MISSION</a></li>
+            <li><a href="#">ABOUT</a></li>
+            <li><a href="#">SERVICES</a></li>
+            <li><a href="#">TEAM</a></li>
+          </ul>
         </div>
       </div>
-    </section>
-    <!-- ABOUT BOX -->
+    </nav>
+    <!-- ABOUT US MENU -->
 
-    <!-- ARTISTRY BOX -->
-    <section class="artistry-box">
-      <div class="container-fluid bg-green">
-        <div class="row">
-          <div class="col-md-12">
-            <h1>Our Artistry</h1>
-            <p>Unlike social media, we are an exclusive Environment related infotainment and media platform for you, to Connect, Discuss and Share the knowledge and information which might positively or negatively or is currently impacting  the environment and everyone on it, for others to take a note of.</p>
-            <p>Our service portfolio which is due launch soon, aims to help every segment of the audience and  aspire to be instrumental in providing a considerable push to growing  momentum towards Global sustainability and sustainable development .</p>
-            <p><b>“We must be the change , we wish to see in the world”- Mahatma Gandhi</b></p>
-          </div>
-        </div>
-      </div>
-    </section>
-    <!-- ARTISTRY BOX -->
 
-    <!-- TEAM BOX -->
-    <section class="about-team-box">
-      <div class="container-fluid bg-green">
-        <div class="row">
-          <div class="col-sm-12">
-            <h1 class="text-center">Team</h1>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-4 team-member">
-            <img src="http://worldartsme.com/images/business-person-clipart-1.jpg" alt="" class="img-responsive">
-            <h5 class="text-center">Kamaljit Deka, Assam</h5>
-            <h5 class="text-center"><strong>Founder, CEO</strong> </h5>
-          </div>
-          <div class="col-md-4 team-member">
-            <img src="http://worldartsme.com/images/business-person-clipart-1.jpg" alt="" class="img-responsive">
-            <h5 class="text-center">Sahaya Christen, Tamilnadu</h5>
-            <h5 class="text-center"><strong>Head of Marketing</strong> </h5>
-          </div>
-          <div class="col-md-4 team-member">
-            <img src="http://worldartsme.com/images/business-person-clipart-1.jpg" alt="" class="img-responsive">
-            <h5 class="text-center">Shruti Sagar, Orissa</h5>
-            <h5 class="text-center"><strong>Head of Sales</strong> </h5>
-          </div>
-        </div>
+  <!-- MISSION SECTION -->
+  <div class="container-fluid mission-section">
+    <h1 class="text-center">Welcome to Eco-Bes</h1>
+    <div class="row">
+      <div class="col-md-4 bg-green mission-box">
+        <h2 class="text-center">Mission</h2>
+        <p class="text-center">"Empowerment Through Wisdom, to encourage environmental resilience across the globe."</p>
       </div>
-    </section>
-    <!-- TEAM BOX -->
+    </div>
+  </div>
+  <!-- MISSION SECTION -->
 
-    <!-- TESTIMONIAL BOX -->
-    {{-- <section class="testimonial-box">
-      <div class="container bg-green">
-        <div class="row">
-          <div class="col-md-2">
-            <img src="https://coin-hero.de/wp-content/uploads/2017/12/nxt.png" alt="" class="img-responsive">
-            <h5 class="text-center">Testimonial</h5>
-          </div>
+  <!-- ABOUT SECTION -->
+  <section class="about-services-n">
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>About Us</h1>
+          <p>Our mission is what drives us to take this enterprising path to Inspire, Motivate and at the same time educate people of all ages across the globe, to become consciously aware and well informed about the current and predicted Global  climatic and environmental status. This we strongly believe has the potential to help in building and establishing an unified perspective about the urgency and need for sustainable development practices followed by actions.</p>
+          <p><b>“We are the first generation to feel the effect of climate change and the last generation who can do something about it”-Barack Obama</b></p>
+          <br>
+          <br><br>
+          <h1>Services</h1>
+          <p>Unlike social media, we are an exclusive Environment related infotainment and media platform for you, to Connect, Discuss and Share the knowledge and information which might positively or negatively or is currently impacting  the environment and everyone on it, for others to take a note of. Our service portfolio which is due launch soon, aims to help every segment of the audience and  aspire to be instrumental in providing a considerable push to growing  momentum towards Global sustainability and sustainable development .</p>
+          <p><b>“We must be the change, we wish to see in the world”- Mahatma Gandhi</b></p>
         </div>
       </div>
-    </section> --}}
-    <!-- TESTIMONIAL BOX -->
+    </div>
+  </section>
+  <!-- ABOUT SECTION -->
 
-    <!-- ABOUT BOTTOM -->
-    <section class="about-page-bottom">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <h3>Address: Maligaon, Guwahati, Assam</h3>
-            <h3>Email: kamal@ecobes.in</h3>
-            <h3>Phone:9886528671</h3>
+  <!-- TESTIMONIAL SECTION -->
+  <section class="testimonial-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+              <div class="item active">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-4">
+                      <img src="https://justatic.com/v/20190211135732/shared/images/icons/placeholders/profile.png" alt="" class="img-responsive">
+                      <h3>Kamaljit Deka</h3>
+                      <h3>Fouder & CEO</h3>
+                    </div>
+                    <div class="col-md-7">
+                      <p>Kamaljit deka is the Founder and CEO of Ecobes Technologies Pvt Ltd. An Ex-Employee of world’s biggest
+Ed-Tech firm Byju’s , Kamal has a professional experience of 3+ Years and he is currently heading the
+Strategy &amp; Operations of Eco-Bes. As an employee, Kamal with his creative initiatives and management
+skills, has always played a key role in developing systems, which improved the overall productivity
+efficiency and of the team and the company he worked in. </p>
+                      <p>After finishing schooling from his hometown Guwahati, Kamal persued his B.Tech in Automobile Engg.
+from Manipal Institute of Technology, Karnataka in the Batch of 2016 and then began his professional
+career from Bangalore.</p>
+                      <p>Inspite of the fact that People do not like change, Kamal is attempting to bring one because he believes
+that “Changes are uncomfortable but often necessary for the greater good of everyone and everything
+around including themselves.”</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="item ">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-4 col-md-offset-1">
+                      <img src="https://cdn1.iconfinder.com/data/icons/technology-devices-2/100/Profile-512.png" alt="" class="img-responsive">
+                      <h3>Kamaljit Deka</h3>
+                      <h3>Fouder & CEO</h3>
+                    </div>
+                    <div class="col-md-5">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right"></span>
+              <span class="sr-only">Next</span>
+            </a>
           </div>
         </div>
       </div>
-    </section>
-    <!-- ABOUT BOTTOM -->
+    </div>
+  </section>
+  <!-- TESTIMONIAL SECTION -->
+
+  <!-- CONTACT SECTION -->
+  <section class="contact-section">
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+          <h1>Contact</h1>
+          <p>Address: Maligaon, Guwahati, Assam</p>
+          <p>Email: kamal@ecobes.in</p>
+          <p>Phone:9886528671</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- CONTACT SECTION -->
+
+  <!-- FOOTER -->
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 col-md-offset-1 text-center">
+          <img src="{{ url('img/logo.png') }}" alt="Ecobes" class="img-responsive">
+          <h2 class="text-center">Follow Us On</h2>
+          <i class="fa fa-facebook"></i>
+          <i class="fa fa-twitter"></i>
+          <i class="fa fa-linkedin"></i>
+          <i class="fa fa-instagram"></i>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- FOOTER -->
 
   </body>
 
